2e18d9de6c789b2e1bc80abf522b5a88f16f82ee
kasperscottjensen <kasperscott94@gmail.com>
2024-05-13 21:24:53 +0200

    initial

---
+++ src/app.svelte
@@ -0,0 +1,67 @@
+<script>
+    // @ts-nocheck
+
+        import './css/main.css'
+        import './css/tailwind.css'
+        import '../node_modules/flowbite/dist/flowbite.min.js'
+        import '../firebase.config.js'
+    
+        import { Router, Route } from 'svelte-routing'
+        import { onMount } from 'svelte'
+    
+        import Home from './pages/home.svelte'
+    
+        // ROUTES FOR NAVIGATION
+    
+        const routes = [
+            { path: '*', component: null },
+            { path: '/', component: Home },
+    
+            { path: '/products', component: null },
+            { path: '/products/:id', component: null },
+    
+            { path: '/applications', component: null },
+            { path: '/applications/:id', component: null },
+    
+            { path: '/knowledge-center', component: null },
+            { path: '/knowledge-center/:id', component: null },
+    
+            { path: '/about', component: null },
+    
+            { path: '/network', component: null },
+            { path: '/network/:id', component: null },
+    
+            { path: '/support', component: null },
+            
+            { path: '/contact', component: null },
+            { path: '/privacy-policy', component: null },
+            { path: '/terms-and-conditions', component: null },
+        ]
+    
+        // INIT STATE AND LOAD HANDLING
+    
+        let isLoading = true
+    
+        onMount(async () => {
+            try {
+                //
+            } finally {
+                isLoading = false
+            }
+        })
+    </script>
+    
+    <!-- ROUTER -->
+    
+    {#if isLoading}
+        <!-- -->
+    {:else}
+        <Router>
+            {#each routes as { path, component }}
+            <Route {path} let:params>
+                <svelte:component this={component} {...params} />
+            </Route>
+            {/each}
+        </Router>
+    {/if}
+    
---
+++ src/pages/home.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Navbar from '../components/header/navbar.svelte'
+    import Hero from '../components/home/hero.svelte'
+    import Presentation from '../components/home/presentation.svelte'
+    import Features from '../components/home/features.svelte'
+    import CTA from '../components/home/cta.svelte'
+</script>
+
+<Navbar />
+<Hero />
+<Presentation />
+<Features />
+<CTA />
